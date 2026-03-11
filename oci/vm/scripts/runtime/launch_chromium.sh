5806f4bdd4515f52eae5bf576cb97fa1e32bf2f1
Papuna Gagnidze <pgagnidze@pm.me>
2026-03-11 23:28:24 +0400

    fix(vm): restore dbus-launch with env file, add crash reporter flag, revert resolution, remove unused build args

--- oci/vm/scripts/runtime/launch_chromium.sh
+++ oci/vm/scripts/runtime/launch_chromium.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Single source of truth for manual chromium launch flags.
 # Used by sway keybinding and waybar button.
+[ -f /tmp/dbus-env ] && . /tmp/dbus-env
 PROFILE="/home/${USERNAME:-bbuser}/.browserbird/browser-profile"
 exec chromium \
   --user-data-dir="$PROFILE" \
@@ -9,5 +10,6 @@ exec chromium \
   --in-process-gpu \
   --no-sandbox \
   --disable-dev-shm-usage \
+  --disable-crash-reporter \
   --ozone-platform=wayland \
   --disable-blink-features=AutomationControlled
--- oci/vm/scripts/runtime/sway_run.sh
+++ oci/vm/scripts/runtime/sway_run.sh
@@ -2,4 +2,8 @@
 export WLR_BACKENDS=headless
 export WLR_LIBINPUT_NO_DEVICES=1
 
-exec dbus-run-session sway --config /home/${USERNAME:-bbuser}/.config/sway/config
+eval $(dbus-launch --sh-syntax)
+export DBUS_SESSION_BUS_ADDRESS
+echo "DBUS_SESSION_BUS_ADDRESS=$DBUS_SESSION_BUS_ADDRESS" > /tmp/dbus-env
+
+exec sway --config /home/${USERNAME:-bbuser}/.config/sway/config
